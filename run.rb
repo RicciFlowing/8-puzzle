a98f0227fc4d2a50937cf53f17832019953b9507
RicciFlowing <benjamin@mathe-sellin.de>
2016-05-25 20:28:46 +0200

    Added a Node and a Tree class

--- run.rb
+++ run.rb
@@ -3,13 +3,41 @@ require 'benchmark'
 
 
 puzzle = Puzzle.new(positions: [1,2,3,4,5,6,7,8,nil], dimension: 3)
-puzzle2  = Puzzle.new(positions: [1,2,3,4,5,6,7,8,nil].shuffle, dimension: 3)
+puzzle2  = Puzzle.new(positions: [1,8,5,4,nil,2,7,6,3], dimension: 3)
+puzzle3  = Puzzle.new(positions: [11,5,12,14,15,2,nil,9,13,7,6,1,3,10,4,8], dimension: 4)
 
 #[].sort{ |x,y| y.priority <=> x.priority }
 #p puzzle.count_false_positions
 #Element = Struct.new(:data, :priority)
 
-puzzles = [puzzle2]
+puzzles = [puzzle3]
+
+class Node
+  attr_accessor :data, :previous
+  def initialize(args)
+    @previous = args.fetch(:previous)
+    @data     = args.fetch(:data)
+  end
+
+  def append
+    Shuffle.next(self)
+  end
+end
+
+class Tree
+  def initialize(args)
+    @nodes = args.fetch(:nodes).to_a
+  end
+
+  def append(node)
+  end
+
+  def root
+    @nodes.bsearch {|x| x.previous = nil}
+  end
+end
+
+Tree.new(Node.new(previous == nil))
 
 # Breadthsearch (restrictid)
 
@@ -37,7 +65,7 @@ def heuristic(puzzles, count)
    end
    # Put new puzzles at the end of the array
    puzzles += new_puzzles
-   puzzles = puzzles.sort{ |x,y| x.steps <=> y.steps }
+   puzzles = puzzles.sort{ |x,y| x.count_false_positions <=> y.count_false_positions }
   end
   p puzzles.count
   p puzzles.pop.count_false_positions
@@ -65,4 +93,4 @@ def heuristic_a(puzzles, count)
   return false
 end
 
-puts Benchmark.measure { p heuristic(puzzles, 3000) }
+#puts Benchmark.measure { p heuristic_a(puzzles, 2000) }
