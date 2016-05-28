81dc5cb51475cf574b398886b8e6208236ccd437
RicciFlowing <benjamin@mathe-sellin.de>
2016-05-28 20:31:25 +0200

    Added a PriorityQue Class

--- p_queue.rb
+++ p_queue.rb
@@ -1,3 +1,29 @@
-#[].sort{ |x,y| y.priority <=> x.priority }
 
-Element = Struct.new(:data, :priority)
+class Queue
+  def initialize(args)
+    @data = args.fetch(:data) || []
+  end
+
+  def get
+    @data.shift
+  end
+
+  def add(new_elements)
+    @data += new_elements.to_a
+  end
+end
+
+class PriorityQueue < Queue
+  def initialize(args)
+    super(args)
+  end
+
+  def get
+    @data.shift
+  end
+
+  def add(new_elements)
+    @data += new_elements.to_a
+    @data.sort!{ |x,y| (x.heuristic) <=> (y.heuristic) }
+  end
+end
