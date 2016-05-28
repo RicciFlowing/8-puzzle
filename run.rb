e8ddde505ec734be7198aa8b7462c9f3bb928bf8
RicciFlowing <benjamin@mathe-sellin.de>
2016-05-28 20:32:00 +0200

    encaspulatd the search allgorithm in ibjects

--- run.rb
+++ run.rb
@@ -1,114 +1,58 @@
 require_relative './puzzle.rb'
+require_relative './p_queue.rb'
+require_relative './tree.rb'
 require 'benchmark'
 
 
 puzzle = Puzzle.new(positions: [1,2,3,4,5,6,7,8,nil], dimension: 3)
-puzzle2  = Puzzle.new(positions: [1,8,5,4,nil,2,7,6,3], dimension: 3)
-puzzle3  = Puzzle.new(positions: [11,5,12,14,15,2,nil,9,13,7,6,1,3,10,4,8], dimension: 4)
-
-#[].sort{ |x,y| y.priority <=> x.priority }
-#p puzzle.count_false_positions
-#Element = Struct.new(:data, :priority)
+puzzle2  = Puzzle.new(positions: [1,2,3,4,5,6,nil,7,8], dimension: 3)
+puzzle3  = Shuffle.shuffle(puzzle, 20)
+puzzle4  = Puzzle.new(positions: [11,5,12,14,15,2,nil,9,13,7,6,1,3,10,4,8], dimension: 4)
 
 puzzles = [puzzle3]
 
-class Node
-  attr_accessor :data, :previous, :children
+
+class BreadthSearch
   def initialize(args)
-    @previous = args.fetch(:previous)
-    @data     = args.fetch(:data)
-    @children = args.fetch(:children, nil)
+    @tree = args.fetch(:tree)
+    @puzzles = Queue.new(data: @tree.root)
   end
 
-  def to_a
-    [self]
+  def find(count)
+    count.times do |i|
+     new_puzzles = @puzzles.get.children
+     if new_puzzles.inject(false){|b, current| b || current.correct? }
+       p "Victory"
+       return i
+     end
+     @puzzles.add(new_puzzles)
+    end
+    return false
   end
 end
 
-class PuzzleNode < Node
+class HeuristicASearch
   def initialize(args)
-    super(args)
+    @tree = args.fetch(:tree)
+    @puzzles = PriorityQueue.new(data: @tree.root)
   end
 
-  def add_children
-    @children = Shuffle.next(@data).map!{ |x| Node.new(data: x, previous: self) }
-  end
-end
-
-
-class Tree
-  attr_reader :root
-
-  def initialize(args)
-    @root = args.fetch(:root).to_a
-  end
-
-  def children(node)
-    node.children
-  end
-
-  def parent(node)
-    node.previous
-  end
-end
-
-root = PuzzleNode.new(data: puzzle2, previous: nil)
-p Tree.new(root: Node.new(data: puzzle2, previous: nil))
-p root.children
-p root.add_children
-p root.children
-# Breadthsearch (restrictid)
-
-def breadth_search(puzzles, count)
-  count.times do |i|
-   next_puzzle = puzzles.shift
-   new_puzzles = Shuffle.next(next_puzzle)
-   if new_puzzles.inject(false){|b, current| b || current.correct? }
-     p "Victory"
-     return i
-   end
-   # Put new puzzles at the end of the array
-   puzzles += new_puzzles
+  def find(count)
+    count.times do |i|
+     new_puzzles = @puzzles.get.children
+     if new_puzzles.inject(false){|b, current| b || current.correct? }
+       p "Victory"
+       return i
+     end
+     # Put new puzzles at the end of the array
+     @puzzles.add(new_puzzles)
+    end
+    return false
   end
 end
 
 
-def heuristic(puzzles, count)
-  count.times do |i|
-   next_puzzle = puzzles.shift
-   new_puzzles = Shuffle.next(next_puzzle)
-   if new_puzzles.inject(false){|b, current| b || current.correct? }
-     p "Victory"
-     return i
-   end
-   # Put new puzzles at the end of the array
-   puzzles += new_puzzles
-   puzzles = puzzles.sort{ |x,y| x.count_false_positions <=> y.count_false_positions }
-  end
-  p puzzles.count
-  p puzzles.pop.count_false_positions
-  p puzzles.pop.steps
-  #puzzles.each { |pu| p pu.to_s}
-  return false
-end
+root = PuzzleNode.new(data: puzzle3, previous: nil)
 
-
-
-def heuristic_a(puzzles, count)
-  count.times do |i|
-   next_puzzle = puzzles.shift
-   new_puzzles = Shuffle.next(next_puzzle)
-   if new_puzzles.inject(false){|b, current| b || current.correct? }
-     p "Victory"
-     return i
-   end
-   # Put new puzzles at the end of the array
-   puzzles += new_puzzles
-   puzzles = puzzles.sort{ |x,y| (x.steps + x.count_false_positions) <=> (y.steps + y.count_false_positions) }
-  end
-  p puzzles.count
-  p puzzles.pop.count_false_positions
-  return false
-end
-
-#puts Benchmark.measure { p heuristic_a(puzzles, 2000) }
+puts Benchmark.measure { p BreadthSearch.new(tree: Tree.new(root: root)).find(2000) }
+puts Benchmark.measure { p HeuristicASearch.new(tree: Tree.new(root: root)).find(2000) }
